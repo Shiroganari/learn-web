96c697ff0b04735dab5c15646e17b44d855d70d0
Shiroganari <shiroganari@gmail.com>
2025-07-27 16:32:15 +0700

    ulbi-react | 007 - create ThemeContext

--- ulbi-react/src/App.tsx
+++ ulbi-react/src/App.tsx
@@ -2,13 +2,17 @@ import { Route, Routes, Link } from 'react-router-dom';
 import { Suspense } from 'react';
 import AboutPageAsync from './pages/AboutPage/AboutPage.async';
 import HomePageAsync from './pages/HomePage/HomePage.async';
-import './index.scss';
+import { useTheme } from './theme/useTheme';
+import './styles/index.scss';
 
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
             <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>\
+            <Link to={'/about'}>О сайте</Link>
+            <button onClick={toggleTheme}>Toggle theme</button>
 
             <Suspense fallback={'...Loading'}>
                 <Routes>
---
+++ ulbi-react/src/theme/ThemeProvider.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, useState } from 'react';
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext';
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+interface ThemeProviderProps {
+    children: ReactNode;
+}
+
+const ThemeProvider = ({ children }: ThemeProviderProps) => {
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+    return (
+        <ThemeContext value={{ theme, setTheme }}>
+            {children}
+        </ThemeContext>
+    );
+};
+
+export default ThemeProvider;
