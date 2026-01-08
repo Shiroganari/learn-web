82cd04286efa21853a4f1d44856daa8c6d0d04bb
Shiroganari <shiroganari@icloud.com>
2026-01-08 18:13:30 +0700

    ulbi-react | 009 - implement classNames helper method

--- ulbi-react/src/App.tsx
+++ ulbi-react/src/App.tsx
@@ -4,12 +4,13 @@ import AboutPageAsync from './pages/AboutPage/AboutPage.async';
 import HomePageAsync from './pages/HomePage/HomePage.async';
 import { useTheme } from './theme/useTheme';
 import './styles/index.scss';
+import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames('app', {}, [theme, 'cls1'])}>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <button onClick={toggleTheme}>Toggle theme</button>
