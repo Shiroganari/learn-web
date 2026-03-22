e35d1b5049073884794f6d826ed9482452f76c56
Shiroganari <shiroganari@icloud.com>
2026-03-22 18:24:27 +0700

    ulbi-react | 016 - enable HotModuleReplacementPlugin

--- ulbi-react/config/build/buildPlugins.ts
+++ ulbi-react/config/build/buildPlugins.ts
@@ -18,6 +18,7 @@ export default function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ];
 }
