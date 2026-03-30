4b16cf279c284becbd0ef5284d3377a63fc76943
Shiroganari <shiroganari@icloud.com>
2026-03-30 19:39:11 +0700

    ulbi-react | 023 - bundle analyzer

--- ulbi-react/config/build/buildPlugins.ts
+++ ulbi-react/config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export default function buildPlugins({
   paths,
@@ -20,5 +21,8 @@ export default function buildPlugins({
       __IS_DEV__: JSON.stringify(isDev)
     }),
     new webpack.HotModuleReplacementPlugin(),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
+    })
   ];
 }
